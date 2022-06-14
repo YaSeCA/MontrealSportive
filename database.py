753afd8647c473c339f011065faa4b240216f1ea
Yacine Sehboub <sehboub_yacine@hotmail.fr>
2022-06-13 23:00:59 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3 as sq
 
+
 class Database:
     def __init__(self):
         self.connection = None
@@ -13,6 +14,10 @@ class Database:
         if self.connection is not None:
             self.connection.close()
 
+    """
+    Initiate the database with all the tables.
+    """
+
     def exc_sql(self):
         connection = self.get_connection()
         cursor = connection.cursor()
@@ -21,28 +26,39 @@ class Database:
         cursor.executescript(sql_read)
         connection.commit()
 
-    #Installations aquatiques
-    def add_aquatique(self,id_uev,type_,nom,arrondisse,addresse,propriete,
-                    gestion,point_x,point_y,equipeme,long_,lat):
+    # Installations aquatiques
+
+    """
+    Add all aquatique installations in the database.
+    """
+
+    def add_aquatique(self, id_uev, type_, nom, arrondisse, addresse,
+                      propriete, gestion, point_x, point_y, equipeme,
+                      long_, lat):
         connection = self.get_connection()
 
-        connection.execute("""INSERT OR REPLACE INTO aquatique(id_uev, type, 
-                            nom, arrondisse, addresse, propriete, gestion, 
+        connection.execute("""INSERT OR REPLACE INTO aquatique(id_uev, type,
+                            nom, arrondisse, addresse, propriete, gestion,
                             point_x, point_y, equipeme, long_, lat)
-                            VALUES(:id_uev, :type, :nom, :arrondisse, 
-                            :addresse, :propriete, :gestion, :point_x, 
+                            VALUES(:id_uev, :type, :nom, :arrondisse,
+                            :addresse, :propriete, :gestion, :point_x,
                             :point_y, :equipeme, :long_, :lat)""",
-                            {'id_uev':id_uev,'type':type_,'nom':nom,
-                            'arrondisse':arrondisse,'addresse':addresse,
-                            'propriete':propriete,'gestion':gestion,
-                            'point_x':point_x,'point_y':point_y,
-                            'equipeme':equipeme,'long_':long_,'lat':lat})
+                           {'id_uev': id_uev, 'type': type_, 'nom': nom,
+                            'arrondisse': arrondisse, 'addresse': addresse,
+                            'propriete': propriete, 'gestion': gestion,
+                            'point_x': point_x, 'point_y': point_y,
+                            'equipeme': equipeme, 'long_': long_, 'lat': lat})
         connection.commit()
+
+    """
+    Get all aquatique installations from a specific arrondissement.
+    """
 
     def get_aquatiques_arrondissement(self, arrondissement):
         cursor = self.get_connection().cursor()
-        cursor.execute("SELECT * FROM aquatique WHERE arrondisse=:arrondissement", 
-                        {'arrondissement':arrondissement})
+        cursor.execute("""SELECT * FROM aquatique
+                       WHERE arrondisse=:arrondissement""",
+                       {'arrondissement': arrondissement})
         fetched_aquatiques = cursor.fetchall()
         return [{
                 "ID_UEV": ligne[1],
@@ -59,7 +75,11 @@ class Database:
                 "LAT": ligne[12],
                 }
                 for ligne in fetched_aquatiques
-            ]
+                ]
+
+    """
+    Get all aquatique installations ordered by ascending name.
+    """
 
     def get_all_aquatiques(self):
         cursor = self.get_connection().cursor()
@@ -80,12 +100,16 @@ class Database:
                 "LAT": ligne[12],
                 }
                 for ligne in fetched_installations
-            ]
-    
+                ]
+
+    """
+    Get all aquatique installations with a specific name.
+    """
+
     def get_aquatiques_nom(self, nom):
         cursor = self.get_connection().cursor()
-        cursor.execute("SELECT * FROM aquatique WHERE nom=:nom", 
-                        {'nom':nom})
+        cursor.execute("SELECT * FROM aquatique WHERE nom=:nom",
+                       {'nom': nom})
         fetched_aquatiques = cursor.fetchall()
         return [{
                 "ID_UEV": ligne[1],
@@ -102,27 +126,16 @@ class Database:
                 "LAT": ligne[12],
                 }
                 for ligne in fetched_aquatiques
-            ]
-    #Installations aquatiques
+                ]
+    # Installations aquatiques
 
-    #Patinoires
-    def add_patinoires(nom_arr, nom_pat, date_heure, ouvert, deblaye, arrose, resurface):
-        pass
+    # Glissades
+    """
+    Add all glissade in the database.
+    """
 
-    def get_patinoires_arrondissement(self, arrondissement):
-        pass
-
-    def get_all_patinoires(self):
-        pass
-
-    def get_patinoires_nom(self, nom):
-        pass
-
-    #Patinoires
-
-    #Glissades
-    def insert_glissade(self,nom,nom_arr,cle,date_maj,ouvert,
-        deblaye,condition):
+    def insert_glissade(self, nom, nom_arr, cle, date_maj, ouvert,
+                        deblaye, condition):
 
         connection = self.get_connection()
         connection.execute(
@@ -135,10 +148,14 @@ class Database:
         )
         connection.commit()
 
+    """
+    Get all glissades from a specific arrondissement.
+    """
+
     def get_glissade_arrondissement(self, arrondissement):
         cursor = self.get_connection().cursor()
-        cursor.execute("SELECT * FROM glissade WHERE nom_arr=:arrondissement", 
-                        {'arrondissement':arrondissement})
+        cursor.execute("SELECT * FROM glissade WHERE nom_arr=:arrondissement",
+                       {'arrondissement': arrondissement})
         fetched_installations = cursor.fetchall()
         return [{
                 "nom": ligne[1],
@@ -150,7 +167,11 @@ class Database:
                 "condition": ligne[7],
                 }
                 for ligne in fetched_installations
-            ]
+                ]
+
+    """
+    Get all glissades ordered by ascending name.
+    """
 
     def get_all_glissades(self):
         cursor = self.get_connection().cursor()
@@ -166,12 +187,16 @@ class Database:
                 "condition": ligne[7],
                 }
                 for ligne in fetched_installations
-            ]
+                ]
+
+    """
+    Get all glissades with a specific name.
+    """
 
     def get_glissades_nom(self, nom):
         cursor = self.get_connection().cursor()
-        cursor.execute("SELECT * FROM glissade WHERE nom=:nom", 
-                        {'nom':nom})
+        cursor.execute("SELECT * FROM glissade WHERE nom=:nom",
+                       {'nom': nom})
         fetched_installations = cursor.fetchall()
         return [{
                 "nom": ligne[1],
@@ -183,27 +208,16 @@ class Database:
                 "condition": ligne[7],
                 }
                 for ligne in fetched_installations
-            ]
+                ]
+    # Glissades
 
-    #Glissades
+    """
+    Get all aquatique installations and all glissades with a specific name.
+    """
 
     def get_all_names(self):
         cursor = self.get_connection().cursor()
-        cursor.execute("SELECT nom FROM aquatique UNION ALL SELECT nom FROM glissade ORDER BY nom ASC")
+        cursor.execute("""SELECT nom FROM aquatique UNION
+                        ALL SELECT nom FROM glissade ORDER BY nom ASC""")
         fetched_names = cursor.fetchall()
         return [ligne[0]for ligne in fetched_names]
-
-
-    def insert_patinoire(
-        self, nom_arr, nom_pat, date_heure, ouvert, deblaye, arrose, resurface
-    ):
-        connection = self.get_connection()
-        connection.execute(
-            (
-                """insert or replace into patinoire(nom_arr,nom_pat,date_heure,
-                ouvert, deblaye, arrose, resurface)"""
-                " values(?, ?, ?, ?, ?, ?, ?)"
-            ),
-            (nom_arr, nom_pat, date_heure, ouvert, deblaye, arrose, resurface),
-        )
-        connection.commit()
