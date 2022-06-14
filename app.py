08b9a64a260dfbf33e074c40f3c230a3c39a6faa
Yacine Sehboub <sehboub_yacine@hotmail.fr>
2022-06-13 23:00:38 -0400

    Update app.py for pycode style

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, render_template, g, redirect
 from flask import make_response, Response, jsonify
 
@@ -47,11 +46,13 @@ def home():
     return render_template('accueil.html')
 
 
-#A1
+# A1
 """
 Download the files from the URLs and parse theme to a panda dataframe.
 Then add the data to the database.
 """
+
+
 @app.route("/get-data")
 def download_data():
     with app.app_context():
@@ -60,34 +61,25 @@ def download_data():
             "e136158133f2/resource/cbdca706-569e-4b4a-805d-9af73af03b14/"
             "download/piscines.csv"
         )
-        patinoire = (
-            "https://data.montreal.ca/dataset/225ac315-49fe-476f-95bd-"
-            "a1ce1648a98c/resource/5d1859cc-2060-4def-903f-db24408bacd0/"
-            "download/l29-patinoire.xml"
-        )
+
         glissade = (
-            "http://www2.ville.montreal.qc.ca/services_citoyens/pdf_transfert/L29_GLISSADE.xml"
+            "http://www2.ville.montreal.qc.ca/services_citoyens/"
+            "pdf_transfert/L29_GLISSADE.xml"
         )
+
         get_db().exc_sql()
 
         aquatique_array = parse_aquatique(aquatique)
         add_aquatique_to_db(aquatique_array)
 
-        # list_patinoire = urllib.request.urlopen(patinoire).read()
-        # tree_patinoire = ET.fromstring(list_patinoire)
-        # createListPatinoire(tree_patinoire)
-
         list_glissade = urllib.request.urlopen(glissade).read()
         tree_glissade = ET.fromstring(list_glissade)
         createListGlissade(tree_glissade)
 
-        #root = parse_xml(glissade)
-        #add_glissade_to_db(root)
-
         return redirect("/"), 200
 
 
-#A2
+# A2
 """
 Data import from point A1 is done automatically
 every day at midnight using a BackgroundScheduler.
@@ -98,10 +90,12 @@ every day at midnight using a BackgroundScheduler.
 
 """
 Prints in console the data from the DB.
-Replace the function download_data() with verify() 
-in add_job(), then change the hour/minuite/second 
+Replace the function download_data() with verify()
+in add_job(), then change the hour/minuite/second
 values to try the background scheduler.
 """
+
+
 @app.route("/try-scheduler")
 def verify():
     array = pd.DataFrame(get_db().get_all_aquatiques())
@@ -123,75 +117,60 @@ def terminate():
     lambda: sched.shutdown()
 
 
-#A3
+# A3
 """
 Shows the documentation for all REST APIs.
 """
+
+
 @app.route("/doc")
 def doc():
     return render_template("doc.html")
 
 
-#A4
+# A4
 """
 Returns a JSON array of all the facilities from a specified area.
 """
+
+
 @app.route("/api/installations", methods=['GET'])
 def get_install_where_arrondissement():
     arrondissement = request.args.get("arrondissement")
 
     if arrondissement is None:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1), 404    
+        return render_template('404.html', error_msg1=error_msg1), 404
     else:
         aquatiques = get_db().get_aquatiques_arrondissement(arrondissement)
-        patinoires = []
         glissades = get_db().get_glissade_arrondissement(arrondissement)
-        if len(aquatiques) & len(patinoires) & len(glissades):
-            response = {
-                "Installations_Aquatiques": aquatiques,
-                "Patinoires": patinoires,
-                "Glissades": glissades
-            }
-        elif len(aquatiques) & len(patinoires):
-            response = {
-                "Installations_Aquatiques": aquatiques,
-                "Patinoires": patinoires
-            }
-        elif len(aquatiques) & len(glissades):
+        if len(aquatiques) & len(glissades):
             response = {
                 "Installations_Aquatiques": aquatiques,
                 "Glissades": glissades
             }
-        elif len(patinoires) & len(glissades):
-            response = {
-                "Patinoires": patinoires,
-                "Glissades": glissades
-        }
         elif len(aquatiques):
             response = {
                 "Installations_Aquatiques": aquatiques
-        }
-        elif len(patinoires):
-            response = {
-                "Patinoires": patinoires
-        }
+            }
         elif len(glissades):
             response = {
                 "Glissades": glissades
-        }
+            }
         else:
             error_msg2 = True
-            return render_template('404.html', error_msg2=error_msg2, 
-                    arrondissement=arrondissement), 404
+            return render_template('404.html', error_msg2=error_msg2,
+                                   arrondissement=arrondissement), 404
 
     return jsonify(response), 200
 
 
-#A6
+# A6
 """
 Returns a JSON array of all the facilities of a specified name.
 """
+
+
 @app.route("/api/installation-nom", methods=['GET'])
 def get_install_where_name():
     nom = request.args.get("nom")
@@ -201,69 +180,52 @@ def get_install_where_name():
         return render_template('404.html', error_msg1=error_msg1), 404
     else:
         aquatiques = get_db().get_aquatiques_nom(nom)
-        patinoires = []
         glissades = get_db().get_glissades_nom(nom)
-        if len(aquatiques) & len(patinoires) & len(glissades):
-            response = {
-                "Installations_Aquatiques": aquatiques,
-                "Patinoires": patinoires,
-                "Glissades": glissades
-            }
-        elif len(aquatiques) & len(patinoires):
-            response = {
-                "Installations_Aquatiques": aquatiques,
-                "Patinoires": patinoires
-            }
-        elif len(aquatiques) & len(glissades):
+        if len(aquatiques) & len(glissades):
             response = {
                 "Installations_Aquatiques": aquatiques,
                 "Glissades": glissades
             }
-        elif len(patinoires) & len(glissades):
-            response = {
-                "Patinoires": patinoires,
-                "Glissades": glissades
-        }
         elif len(aquatiques):
             response = {
-                "Installations_Aquatiques": aquatiques
+                "Installations_Aquatiques": aquatiques,
             }
-        elif len(patinoires):
-            response = {
-                "Patinoires": patinoires
-        }
         elif len(glissades):
             response = {
                 "Glissades": glissades
-        }
+            }
         else:
             error_msg4 = True
-            return render_template('404.html', error_msg2=error_msg4, 
-                    nom=nom), 404
-                    
+            return render_template('404.html', error_msg2=error_msg4,
+                                   nom=nom), 404
+
     return jsonify(response), 200
 
 
 """
 Returns a JSON array of all the facilities of a specified name.
 """
+
+
 @app.route("/names")
 def get_all_names():
     names = get_db().get_all_names()
     return jsonify(names), 200
 
 
-#C1
+# C1
 """
 Returns a JSON array of all the facilities of a specified type.
 """
+
+
 @app.route("/api/installation-type", methods=['GET'])
 def get_install_where_type():
     type_ = request.args.get("type")
 
     if type_ is None:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1), 404 
+        return render_template('404.html', error_msg1=error_msg1), 404
     elif type_ == "aquatique":
         installations = get_db().get_all_aquatiques()
         if installations:
@@ -272,18 +234,8 @@ def get_install_where_type():
             }
         else:
             error_msg3 = True
-            return render_template('404.html', error_msg3=error_msg3, 
-                    type_=type_), 404
-    elif type_ == "patinoire":
-        installations = []
-        if installations:
-            response = {
-                "Patinoires": installations
-                    }
-        else:
-            error_msg3 = True
-            return render_template('404.html', error_msg3=error_msg3, 
-                    type_=type_), 404
+            return render_template('404.html', error_msg3=error_msg3,
+                                   type_=type_), 404
     elif type_ == "glissade":
         installations = get_db().get_all_glissades()
         if installations:
@@ -292,75 +244,75 @@ def get_install_where_type():
             }
         else:
             error_msg3 = True
-            return render_template('404.html', error_msg3=error_msg3, 
-                    type_=type_), 404
+            return render_template('404.html', error_msg3=error_msg3,
+                                   type_=type_), 404
     else:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1, 
-                type_=type_), 404
+        return render_template('404.html', error_msg1=error_msg1,
+                               type_=type_), 404
 
     return jsonify(response), 200
 
 
-#C2
+# C2
 """
 Returns a XML file of all the facilities of a specified type.
 """
+
+
 @app.route("/api/installation-type-xml", methods=['GET'])
 def get_install_type_xml():
     type_ = request.args.get("type")
 
     if type_ is None:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1), 404 
+        return render_template('404.html', error_msg1=error_msg1), 404
     elif type_ == "aquatique":
         installations = get_db().get_all_aquatiques()
-    elif type_ == "patinoire":
-        installations = []
     elif type_ == "glissade":
-        installations = []
+        installations = get_db().get_all_glissades()
     else:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1, 
-                type_=type_), 404
-        
+        return render_template('404.html', error_msg1=error_msg1,
+                               type_=type_), 404
+
     if installations:
         xmlData = dicttoxml.dicttoxml(installations)
         parsedXML = parseString(xmlData)
         xmlFile = parsedXML.toprettyxml(indent="\t",
-            newl='\n', encoding="utf-8")
+                                        newl='\n', encoding="utf-8")
         response = Response(xmlFile, mimetype="text/xml")
         response.headers.set("Installations sportives", "text/xml")
     else:
         error_msg3 = True
-        return render_template('404.html', error_msg3=error_msg3, 
-                type_=type_), 404
-    
+        return render_template('404.html', error_msg3=error_msg3,
+                               type_=type_), 404
+
     return response, 200
 
 
-#C3
+# C3
 """
 Returns a CSV file of all the facilities of a specified type.
 """
+
+
 @app.route("/api/installation-type-csv", methods=['GET'])
 def get_install_type_csv():
     type_ = request.args.get("type")
 
     if type_ is None:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1), 404 
+        return render_template('404.html', error_msg1=error_msg1), 404
     elif type_ == "aquatique":
         installations = get_db().get_all_aquatiques()
-    elif type_ == "patinoire":
-        installations = []
     elif type_ == "glissade":
-        installations = []
+        installations = get_db().get_all_glissades()
     else:
         error_msg1 = True
-        return render_template('404.html', error_msg1=error_msg1, 
-                type_=type_), 404
-    
+        return render_template('404.html', error_msg1=error_msg1,
+                               type_=type_), 404
+
     if installations:
         csvData = pd.DataFrame(installations)
         response = make_response(csvData.to_csv(sep=",",
@@ -371,37 +323,41 @@ def get_install_type_csv():
                                                 line_terminator="\n"))
     else:
         error_msg3 = True
-        return render_template('404.html', error_msg3=error_msg3, 
-                type_=type_), 404
-    
+        return render_template('404.html', error_msg3=error_msg3,
+                               type_=type_), 404
+
     return response, 200
 
 
 """
-Parses a CSV file contening data of aquatique facilities into a panda 
+Parses a CSV file contening data of aquatique facilities into a panda
 dataframe.
 
 Parameters:
 - url : url to a CSV document
 """
+
+
 def parse_aquatique(url):
-    columns = ['ID_UEV', 'TYPE', 'NOM', 'ARRONDISSE', 'ADRESSE', 'PROPRIETE'
-                , 'GESTION', 'POINT_X', 'POINT_Y', 'EQUIPEME', 'LONG', 'LAT']
+    columns = ['ID_UEV', 'TYPE', 'NOM', 'ARRONDISSE', 'ADRESSE', 'PROPRIETE',
+               'GESTION', 'POINT_X', 'POINT_Y', 'EQUIPEME', 'LONG', 'LAT']
 
     aquatique_array = pd.read_csv(url, names=columns, header=None)
-    #remove the first line and column
-    aquatique_array = aquatique_array.iloc[1: , :]
-    #print(aquatique_array)
+    # remove the first line and column
+    aquatique_array = aquatique_array.iloc[1:, :]
+    # print(aquatique_array)
     return aquatique_array
 
 
 """
-Fills the aquatique SQLite table in db.sql with data from a panda 
+Fills the aquatique SQLite table in db.sql with data from a panda
 dataframe.
 
 Parameters:
 - array : a panda array
 """
+
+
 def add_aquatique_to_db(array):
     for i in array.index:
         id_uev = array['ID_UEV'][i]
@@ -417,72 +373,19 @@ def add_aquatique_to_db(array):
         long_ = array['LONG'][i]
         lat = array['LAT'][i]
         get_db().add_aquatique(id_uev, type_, nom, arrondisse, addresse,
-            propriete, gestion, point_x, point_y, equipeme, long_, lat)
+                               propriete, gestion, point_x, point_y,
+                               equipeme, long_, lat)
 
 
 """
-Parses a XML file contening data of glissade facilities, then returns
-the root of the XML tree
-
-Parameters:
-- xml_file : url to a XML document
-"""
-def parse_xml(xml_file):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    return root
-
-
-"""
-Fills the glissade SQLite table in db.sql with data from a parsed XML 
+Fills the glissade SQLite table in db.sql with data from a parsed XML
 file.
 
 Parameters:
-- root : the root of the XML tree
+- tree_glissade : the root of the XML tree
 """
-def add_glissade_to_db(root):
-    glissades = []
-    for glissade in root:
-        nom, nom_arr, cle, date_maj, ouvert, deblaye, condition = ('',) * 7
-        #glissade_id = glissade.attrib.get('id')
-        for glissade_data in glissade:
-            if glissade_data.tag == 'nom':
-                nom = str(glissade_data.text).strip()
-                print(nom)
 
 
-            # elif glissade_data.tag == 'nom_arr':
-            #     nom_arr = str(glissade_data.text).strip()
-            #     print(nom_arr)
-
-            # elif glissade_data.tag == 'cle':
-            #     cle = str(glissade_data.text).strip()
-            #     print(cle)
-
-            # elif glissade_data.tag == 'date_maj':
-            #     date_maj = str(glissade_data.text).strip()
-            #     print(date_maj)
-
-
-            elif glissade_data.tag == 'ouvert':
-                ouvert = str(glissade_data.text).strip()
-                print(ouvert)
-
-            elif glissade_data.tag == 'deblaye':
-                deblaye = str(glissade_data.text).strip()
-                print(deblaye)
-
-            elif glissade_data.tag == 'condition':
-                condition = str(glissade_data.text).strip()
-                print(condition)
-
-
-        # t = (nom, nom_arr, cle, date_maj, ouvert, deblaye, condition)
-        # glissades.append(t)
-
-        # get_db().get_patinoires_arrondissement(
-        #     nom, nom_arr, cle, date_maj, ouvert, deblaye, condition)
-        
 def createListGlissade(tree_glissade):
     db = get_db()
     for row in tree_glissade.findall("glissade"):
@@ -493,36 +396,15 @@ def createListGlissade(tree_glissade):
         ouvert = row.find("ouvert").text
         deblaye = row.find("deblaye").text
         condition = row.find("condition").text
-        db.insert_glissade(nom, nom_arr, cle, date_maj, ouvert, deblaye, condition)
+        db.insert_glissade(nom, nom_arr, cle, date_maj,
+                           ouvert, deblaye, condition)
 
-        #print(db.get_all_glissades())
+        # print(db.get_all_glissades())
 
     return None
 
-def createListPatinoire(tree_patinoire):
-    db = get_db()
-    for y in tree_patinoire.iter("arrondissement"):
-        nom_arr = y.find("nom_arr").text
-        for z in y.iter("patinoire"):
-            nom_pat = z.find("nom_pat").text
-            for x in z.iter("condition"):
-                date_heure = x.find("date_heure").text
-                ouvert = x.find("ouvert").text
-                deblaye = x.find("deblaye").text
-                arrose = x.find("arrose").text
-                resurface = x.find("resurface").text
-                db.insert_patinoire(
-                    nom_arr.replace(" ", "").replace("\n", ""),
-                    nom_pat,
-                    date_heure,
-                    ouvert,
-                    deblaye,
-                    arrose,
-                    resurface,
-                )
-    return None
 
-#Error handling
+# Error handling
 @app.errorhandler(400)
 def error_400_handler(e):
     defaut = True
